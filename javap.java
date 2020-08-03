9ce2fb4f83d6d30793474490c3ae80e9535c04ea
Admin <biswal81@gmail.com>
2020-08-03 20:34:05 +0530

    IP Address

--- javap.java
+++ javap.java
@@ -1,5 +1,22 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.net.InetAddress;
+class GetMyIPAddress {
+    public static void main(String args[]) throws Exception {
+        /* public static InetAddress getLocalHost()
+         * throws UnknownHostException: Returns the address
+         * of the local host. This is achieved by retrieving
+         * the name of the host from the system, then resolving
+         * that name into an InetAddress. Note: The resolved
+         * address may be cached for a short period of time.
+         */
+        InetAddress myIP = InetAddress.getLocalHost();
+        InetAddress ubuntuIP= InetAddress.getByName("ubuntu-1");
+        /*InetAddress myIP = InetAddress.getByName("ubuntu-2");
+        /* public String getHostAddress(): Returns the IP
+         * address string in textual presentation.
+         */
+        System.out.println("My IP Address is:");
+        System.out.println(myIP.getHostAddress());
+        System.out.println("My ubuntu-1 IP Address is:");
+        System.out.println(ubuntuIP.getHostAddress());
     }
 }
